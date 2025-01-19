0c136fd8f67b4e0a305714622defa1aeac92b5cc
root <root@Ashwin-Laptop.localdomain>
2025-01-19 17:39:08 +0000

    feat:updating the external and automatic variable

---
+++ 2_1_Variables_file_2.c
@@ -0,0 +1,2 @@
+#include<stdio.h>
+short external_variable = 100;// Definition of external variable
--- 2_variables.c
+++ 2_variables.c
@@ -27,6 +27,7 @@ Declaring / Creating Variables :-
         --------------------
     where 'int' - datatype
           'number' - variable
+          '=' - assignment operator (assigns the value to the variable)
           '15' - value
     
 
@@ -37,12 +38,14 @@ void global_variable();
 void static_variable();
 
 int global_variables = 20; // Global Variable
+extern short external_variable; // External Variable
 
 void main()
 {
     printf("Variables Introduction :- \n");
     int number = 15;
-    printf("%d \n", number);
+    printf("Number = %d \n", number);
+    printf("The memory location of the number (%d) is : %p \n", number, &number);
 
     printf("-----------------------------------------------------------------------\n\n");
 
@@ -52,6 +55,8 @@ void main()
     int num1;
     // Assign a value to a variable
     num1 = 26;
+    printf("Num1 = %d \n", num1);
+    printf("Memory Address of Num1 (%d) is : %p \n", num1, &num1);
     
     /*
     Output Variables :- 
@@ -82,11 +87,13 @@ void main()
     // print variables
     printf("%i \n", integer);
     printf("%f \n", floating_point);
+    printf("Memory address of the Floating Point (%f) is : %p \n", floating_point, &floating_point);
     printf("%c \n", character);
+    printf("Memory address of the character (%c) is : %p \n", character, &character);
 
     printf("-----------------------------------------------------------------------\n");
 
-    // To print different types in a single priuntf() function, use the following
+    // To print different types in a single printf() function, use the following
     printf("Print different types in a single printf() function :- \n");
     int variable1 = 256;
     char variable2 = 'A';
@@ -102,23 +109,29 @@ void main()
    printf("Change the Variable Values :- \n");
     int myNum = 15;
     printf("Old 'myNum' value :-  %d \n", myNum);
+    printf("Memory address of Old Num (%d) is : %p \n", myNum, &myNum);
     myNum = 10;
     printf("New 'myNum' value :- %d \n", myNum);
+    printf("Memory address of New Num (%d) is : %p \n", myNum, &myNum);
 
     printf("-----------------------------------------------------------------------\n");
 
     // You can also assign the value of one varable to another variable
-    printf("Assigning the vLaue of one variable to another variable :- \n");
+    printf("Assigning the value of one variable to another variable :- \n");
     printf("Before Change \n");
     float one_value = 12.3;
     float another_value = 2.36;
     printf("One Value :- %f \n", one_value);
     printf("Another Value :- %f \n", another_value);
+    printf("Memory Address of One Value (%f) (before change) is : %p \n", one_value, &one_value);
+    printf("Memory Address of Another Value (%f) (before change) is : %p \n", another_value, &another_value);
     printf("After Change \n");
     // Assign the value of 'another_value' to 'one_value'
     one_value = another_value;
     printf("One Value :- %f \n", one_value);
     printf("Another Value :- %f \n", another_value);
+    printf("Memory Address of One Value (%f) (after change) is : %p \n", one_value, &one_value);
+    printf("Memory Address of Another Value Value (%f) (after change) is : %p \n", another_value, &another_value);
 
     printf("-----------------------------------------------------------------------\n");
 
@@ -134,6 +147,7 @@ void main()
     printf("Y : %d \n", y);
     printf("Z : %d \n", z);
 
+
     printf("-----------------------------------------------------------------------\n");
 
 
@@ -144,6 +158,9 @@ void main()
     printf("A : %d \n", a);
     printf("B : %d \n", b);
     printf("C : %d \n", c);
+    printf("Memory address of A (%d) is : %p \n", a, &a);
+    printf("Memory address of B (%d) is : %p \n", b, &b);
+    printf("Memory address of C (%d) is : %p \n", c, &c);
 
     printf("-----------------------------------------------------------------------\n");
 
@@ -151,7 +168,7 @@ void main()
     /*
     C Variable Names :- 
     ---------------------
-        All the C variables must nbe identified with unique names. These unique names are called as 'identifiers'.
+        All the C variables must be identified with unique names. These unique names are called as 'identifiers'.
     Identifiers can be short names (like 'x' and 'Y') (or) more descriptive names (like 'age', 'sum', 'totalVolume').
 
         NOTE :- It is recommended to use descriptive names in order to create understandable and maintanable code
@@ -177,7 +194,7 @@ void main()
     printf("Grade : %d \n", student_grade);
 
     printf("\n\n------------------------------------------------------ \n");
-    printf("Types of Variables :- ");
+    printf("Types of Variables :- \n");
 
     int local_variable = 10; // Local Variable
     printf("Local Variable :- %d \n", local_variable);
@@ -188,6 +205,57 @@ void main()
     static_variable();
     static_variable();
 
+/*
+Automatic variable :- 
+-----------------------
+    All the variables in C that are declared inside the block are automatic variables by default.
+    We can also explicitly declare an automatic variable using 'auto' keyword.
+    Since By default, the memory of the variable is allocated at the beginning of the block/function and deallocated at the end.
+    So, The 'auto' keyword is very rarely used.
+ */
+    short variable = 10;
+    auto short automatic_variable = 20; // Both are same
+/*
+External Variable :- 
+---------------------
+    An external variable in C is a variable that is declared in one file and used(or refernced) in another file.
+    External Variable enables sharing of variables across multiple files in a program.
+    They are also referred as global variables when they are accessible throughout the program, but their explicit declaration with the 'extern' keyword allows cross file visibility
+
+Key Points about external variables :- 
+----------------------------------------
+(i) Declared Globally :-
+--------------------------
+    External variables are defined outside of all functions, typically at the top of the file
+(ii) Storage Class :- 
+----------------------
+    Declared with the extern keyword in files where they are referenced but not defined.
+    Defined without the extern keyword in the file where the variable is created.
+(iii) Scope :-
+----------------
+    External Variable have global scope and can be accessed by all function in a program
+(iv) Lifetime :-
+------------------
+    Their lifetime is the entire duration of the program's execution.
+
+How External Variable Works :-
+--------------------------------
+    Definition : An external variable is defined in one file without the extern keyword
+    Declaration : The same variable is declared in other files using the extern keyword to access it.
+
+Benefits :-
+-----------
+    It allows variables to share between multiple files in a modular program.
+    Enable communication between different parts of the program
+
+Limitation and Best Practices :-
+----------------------------------
+    Unintended Modification :- External Variable can be modified by any part of the program. Use with caution.
+    Encapsulation :- Avoid excessive use of the external variable. Prefer passing data via function, arguments or use static variables for limited scope
+    Documentation : Clearly document the process, and use of external variables to maintain code readability.
+
+ */
+    printf("External Variable (using extern keyword) is : %hd\n", external_variable);
 }
 
 void global_variable()
@@ -196,9 +264,17 @@ void global_variable()
     printf("Global Variable :- %d \n", global_variables);
 }
 
+/*
+Static Variables :- 
+----------------------
+    A Variable that is declared with the 'static' keyword is called static variable.
+    It retains the value between the multiple function calls 
+ */
 void static_variable()
 {
-    static int static_variables = 10;
+    static int static_variables = 10; // static variable
+    // printf("Memory address of the static variable (%d) (before incrementing) : %p\n", static_variable, &static_variable);
     static_variables++;
     printf("Static Variable :- %d \n", static_variables);
+    // printf("Memory address of the static variable (%d) (After incrementing) : %p\n", static_variable, &static_variable);
 }
