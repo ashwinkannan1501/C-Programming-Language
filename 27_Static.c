398dd9222489e433788677eace9a8253bb8f14ef
root <root@Ashwin-Laptop.localdomain>
2025-05-04 10:59:27 +0000

    New Commit

--- 27_Static.c
+++ 27_Static.c
@@ -1,24 +1,18 @@
 #include<stdio.h>
 
-static int increment() // If the function itself is declared as 'static' function, then the static function is can't accessed by other files
+short increment()
 {
-    short count = 0;
+    static short count = 0;
     count += 1;
     return count;
 }
 
-void main()
+int main()
 {
-    printf("----------Static Variable -------------- \n");
-    
 
-    static short count = increment();
-    count = increment();
+    printf("------------ Static Variable ------------------- \n");
+    printf("%hd \n", increment());
+    printf("%hd \n", increment());
 
-    printf("The current incremented value is : \n");
-
-    for (short index = 1; index <= count; index += 1)
-    {
-        printf("%hd \n", index);
-    }
+    return 0;
 }
